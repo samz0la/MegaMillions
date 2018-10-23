a52c6bbd57a27ee1e3f4aefda089dd6c4d83f323
samuelandazola <szola3@icloud.com>
2018-10-23 09:50:27 -0600

    Initial commit

---
+++ app/src/main/java/samuelandazola/com/megamillions/MainActivity.java
@@ -0,0 +1,54 @@
+package samuelandazola.com.megamillions;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import edu.cnm.deepdive.Generator;
+import edu.cnm.deepdive.MMGenerator;
+import java.security.SecureRandom;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+  private Generator generator;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    generator = new MMGenerator(new SecureRandom());
+    Toolbar toolbar = findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+    FloatingActionButton fab = findViewById(R.id.add_pick);
+    fab.setOnClickListener((view) ->
+        Snackbar.make(view, Arrays.toString(generator.generate()), Snackbar.LENGTH_LONG)
+            .setAction("Action", null).show());
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+}
