bfe5c2a68ae4fd91ba069014c08b809a1be85b51
samuelandazola <szola3@icloud.com>
2018-10-23 22:29:17 -0600

    extracted constants, color resources, and string resources in PickAdapter  Pulled invariants out of loops in PickAdapter  extracted dimension resources and styles in pick_item

--- app/src/main/java/samuelandazola/com/megamillions/PickApplication.java
+++ app/src/main/java/samuelandazola/com/megamillions/PickApplication.java
@@ -6,17 +6,20 @@ import samuelandazola.com.megamillions.model.db.PickDatabase;
 
 public class PickApplication extends Application {
 
+  // TODO Remove
   private PickDatabase database;
 
   @Override
   public void onCreate() {
     super.onCreate();
     Stetho.initializeWithDefaults(this);
+    // TODO Remove database stuff from here; not needed anymore.
     database = PickDatabase.getInstance(this);
   }
 
   @Override
   public void onTerminate() {
+    // TODO Remove database stuff (and this method) from here; not needed anymore.
     PickDatabase.forgetInstance();
     super.onTerminate();
   }
--- app/src/main/java/samuelandazola/com/megamillions/view/PickAdapter.java
+++ app/src/main/java/samuelandazola/com/megamillions/view/PickAdapter.java
@@ -1,9 +1,9 @@
 package samuelandazola.com.megamillions.view;
 
 import android.content.Context;
-import android.graphics.Color;
-import android.os.AsyncTask;
+import android.content.res.Resources;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
@@ -11,7 +11,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import java.util.Arrays;
 import java.util.List;
 import samuelandazola.com.megamillions.R;
 
@@ -20,7 +19,7 @@ public class PickAdapter extends RecyclerView.Adapter<PickAdapter.Holder> {
   private Context context;
   private List<int[]> picks;
 
-  //TODO Modify to take List<PickWithNumbers>.
+  // TODO Modify to take List<PickWithNumbers>.
   public PickAdapter(Context context, List<int[]> picks) {
     this.context = context;
     this.picks = picks;
@@ -29,18 +28,18 @@ public class PickAdapter extends RecyclerView.Adapter<PickAdapter.Holder> {
   @NonNull
   @Override
   public Holder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-  View view = LayoutInflater.from(context).inflate(R.layout.pick_item, viewGroup, false);
-  return new Holder(view);
+    View view = LayoutInflater.from(context).inflate(R.layout.pick_item, viewGroup, false);
+    return new Holder(view);
   }
-
 
   @Override
   public void onBindViewHolder(@NonNull Holder holder, int position) {
-    holder.bind(); //TODO current PickWithNumbers instance
-    if (position % 2 == 1) {
-      holder.itemView.setBackgroundColor(Color.argb(16, 0, 0, 0));
-    }
-    // TODO See if theres a better wat to do alternate-row shading.
+    holder.bind(); // TODO Pass current PickWithNumbers instance.
+    // TODO Note that ternary (or if-else) is needed to deal with re-bound holders.
+    int background = (position % 2 == 0)
+        ? ContextCompat.getColor(context, R.color.pickBackground)
+        : ContextCompat.getColor(context, R.color.pickBackgroundAlternate);
+    holder.itemView.setBackgroundColor(background);
   }
 
   @Override
@@ -49,27 +48,31 @@ public class PickAdapter extends RecyclerView.Adapter<PickAdapter.Holder> {
   }
 
   public class Holder extends RecyclerView.ViewHolder
-      implements View.OnCreateContextMenuListener{
+      implements View.OnCreateContextMenuListener {
 
     private static final int PICK_LENGTH = 6;
-    private TextView[] numbers;
+    private static final String ID_RES_TYPE = "id";
+    private static final String NUM_ID_FORMAT = "num_%d";
 
+    private TextView[] numbers;
 
     public Holder(@NonNull View view) {
       super(view);
+      Resources res = context.getResources();
+      String pkg = context.getPackageName();
       view.setOnCreateContextMenuListener(this);
       numbers = new TextView[PICK_LENGTH];
-      for(int i = 0; i < PICK_LENGTH; i++) {
-        int id = context.getResources().getIdentifier("num_" + i, "id", context.getPackageName());
+      for (int i = 0; i < PICK_LENGTH; i++) {
+        int id = res.getIdentifier(String.format(NUM_ID_FORMAT, i), ID_RES_TYPE, pkg);
         numbers[i] = view.findViewById(id);
       }
     }
 
     private void bind() {
-      //TODO Use PickWithNumbers instance
+      // TODO Use PickWithNumbers instance.
       int[] numbers = picks.get(getAdapterPosition());
-      for(int i = 0; i < numbers.length; i++) {
-        this.numbers[i].setText(Integer.toString(numbers [i]));
+      for (int i = 0; i < numbers.length; i++) {
+        this.numbers[i].setText(context.getString(R.string.pick_number_format, numbers[i]));
       }
     }
 
@@ -78,11 +81,13 @@ public class PickAdapter extends RecyclerView.Adapter<PickAdapter.Holder> {
       menu.add(R.string.delete_pick).setOnMenuItemClickListener((item) -> {
         picks.remove(getAdapterPosition());
         notifyItemRemoved(getAdapterPosition());
+        notifyItemRangeChanged(getAdapterPosition(), getItemCount() - getAdapterPosition());
         return true;
       });
     }
+
   }
 
-  //TODO Create DeleteTask that takes a PickWithNumbers instance.
-}
+  // TODO Create DeleteTask that takes a PickWithNumbers instance.
 
+}
